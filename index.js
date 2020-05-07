f474c29a1c1f82fdf24bffae404747a1b4ff8a09
Valeria Protsan <valeriyapro@gmail.com>
2020-05-06 23:32:33 -0400

    u

--- index.js
+++ index.js
@@ -48,3 +48,41 @@
 // })
 
 //PROMISE is an object that can represent an eventual completion of asynchronous operation
+// let delay = (seconds) => new Promise((results, rejects) => {
+//   // throw new Error('arg')
+//   if (seconds > 3) {
+//     rejects(new Error(`${seconds} is too long`))
+//   }
+//   setTimeout(results, seconds*1000)
+// })
+//
+// delay(2)
+//   .then(()=> console.log('delay has passed'))
+//   .then(() => 42)
+//   .then(number => console.log(`${number}`))
+//   //use CATCH to handle error
+//   .catch(error => console.log(error.message))
+//
+// console.log('End first tick')
+
+//Both promises and callbacks can be handles within node
+let delay = (seconds, callback) => {
+  if (seconds > 3){
+    callback(new Error(`${seconds} is too long`))
+  } else {
+    setTimeout(()=>
+      callback(null, `the ${seconds} delay is over`),
+      seconds
+    )
+  }
+}
+
+delay(4, (error, message) => {
+  if (error){
+    console.log(error.message)
+  } else {
+    console.log(message)
+  }
+})
+
+//Convert callback functions into promises:
